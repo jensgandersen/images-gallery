777ec62acb59b3855a5a74debadc1e793cf84860
Jens Endelt Andersen <jega@bsjviborg.dk>
2021-05-29 23:46:55 +0200

    reset search submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
 const App = () => {
     const [word, setWord] = useState('');
 
     const handleSearchSubmit = (event) => {
         event.preventDefault();
         console.log(word);
+        fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+            .then((result) => result.json())
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        setWord('');
     };
-
-    console.log(word);
 
     return (
         <div>
